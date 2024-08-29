d3fdc30ce384465a2ff7f554850e2e3a178b802e
wrench141 <sidhardhchandra141@gmail.com>
2024-08-29 16:02:31 +0530

    cors blockage removed

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+const corsOptions = {
+    origin: 'https://project-weather-frontend.vercel.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'] 
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 //api endpoints
 app.use("/weather.api.v1/", searchRoutes);
